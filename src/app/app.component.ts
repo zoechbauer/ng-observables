fdd8c601eca382aaf295ae00ec8b18adf5031251
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-17 15:16:00 +0100

    Don't forget to unsubscribe from Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from './user/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   activate = false;
+  activateSub: Subscription;
+
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe((value: boolean) => {
-      this.activate = value;
-    });
+    this.activateSub = this.userService.activatedEmitter.subscribe(
+      (value: boolean) => {
+        this.activate = value;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.activateSub.unsubscribe();
   }
 }
