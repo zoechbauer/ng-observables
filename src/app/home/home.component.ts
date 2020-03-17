3123b37b0d2c2c9ecd2d7417bf28b8f1feae9b99
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-17 11:07:02 +0100

    handling completion of observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,6 +20,9 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
         if (count > 3) {
           observer.error(new Error('Count greater than 3'));
         }
@@ -34,6 +37,9 @@ export class HomeComponent implements OnInit, OnDestroy {
       error => {
         console.log(error);
         alert(error.message);
+      },
+      () => {
+        console.log('completed!');
       }
     );
   }
