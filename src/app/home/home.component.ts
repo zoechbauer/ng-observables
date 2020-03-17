ee8d8b3136fd23bb15e30a5059ca8233d19b79fd
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-17 10:00:39 +0100

    subscribe and unsubscribe to interval observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  firstObsSubscription: Subscription;
+  constructor() {}
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe((count: number) => {
+      console.log(count);
+    });
   }
 
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
